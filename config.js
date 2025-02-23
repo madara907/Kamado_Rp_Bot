92b7014f660aa6c769165b260de30db1d382e921
madara907 <freefireeurope847@gmail.com>
2025-02-23 18:42:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "5ZgFUJQQ#N7RsQyvP8JevgFfma0Bq4XQTd5FzQ1ocZDLAKNHc7CU", 
+SESSION_ID: process.env.SESSION_ID || "1YpRlaAS#GGkmLqrtpsXqoQZLKcrQFH-67zpCveOIaO0NFSJZmaw", 
 MONGODB: process.env.MONGODB || "mongodb+srv://raizen:darki123@cluster0.utsln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //ne pas changer
 };
